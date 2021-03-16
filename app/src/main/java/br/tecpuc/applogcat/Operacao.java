dd34394ddd026dce144bfcf0c852d2c3cb6e6eaa
Giuliano Gevard <giuliano.gevard@tecpuc.com.br>
2021-03-15 22:25:20 -0300

    Initial commit

---
+++ app/src/main/java/br/tecpuc/applogcat/Operacao.java
@@ -0,0 +1,44 @@
+package br.tecpuc.applogcat;
+
+public class Operacao {
+
+    private float operador1;
+    private float operador2;
+    private float operador3;
+
+    public Operacao (float op1, float op2)
+    {
+        this.operador1 = op1;
+        this.operador2 = op2;
+    }
+
+    public float getOperador1()
+    {
+        return this.operador1;
+    }
+
+    public float getOperador2()
+    {
+        return this.operador2;
+    }
+
+    public float Soma()
+    {
+        return operador1 + operador2;
+    }
+
+    public float Subtracao()
+    {
+        return operador1 - operador2;
+    }
+
+    public float Multiplicacao()
+    {
+        return operador1 * operador2;
+    }
+
+    public float Divisao()
+    {
+        return operador1 / operador2;
+    }
+}
